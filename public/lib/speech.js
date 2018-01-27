808fcf7e107e8018659c7bd91e264322453ff23a
progfay <shun1856shun@gmail.com>
2018-01-27 16:52:47 +0900

    feat(speech.js): voice detector that react "あった"

---
+++ public/lib/speech.js
@@ -0,0 +1,20 @@
+var rec = new webkitSpeechRecognition();
+rec.continuous = false;
+rec.interimResults = false;
+rec.lang = 'ja-JP';
+
+var searchStr = /あった|見つけた|コインだ/;
+
+rec.onresult = function(e) {
+    rec.stop();
+    for (var i = e.resultIndex; i < e.results.length; ++i) {
+        if (e.results[i].isFinal && searchStr.test(e.results[i][0].transcript)) {
+            console.log(e.results[i][0].transcript);
+            vm.getCoin();
+        }
+    }
+}
+
+rec.onend = () => { rec.start() };
+
+rec.start();
