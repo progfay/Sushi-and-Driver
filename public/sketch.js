5e746ef5c9561026217162a7fb350a3499ffe7df
progfay <shun1856shun@gmail.com>
2018-01-28 09:54:11 +0900

    feat(sketch.js): add isCoinExist()

--- public/sketch.js
+++ public/sketch.js
@@ -56,6 +56,10 @@ function foundCoin() {
     spawnTimer = SPAWN_COUNT;
 }
 
+function isCoinExist() {
+    return !!coin;
+}
+
 //------------------------------------------------------
 //loop
 (function renderLoop() {
